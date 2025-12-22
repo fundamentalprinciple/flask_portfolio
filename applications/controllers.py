ef1311a69afab534546cd8098a14124eb69b5f87
fundamentalprinciple <princedixit931@gmail.com>
2025-12-22 13:31:42 +0000

    navbar done

--- applications/controllers.py
+++ applications/controllers.py
@@ -1,6 +1,25 @@
 from main import app
 from flask import render_template
 
+from variables import client_name
+
 @app.route('/')
-def home():
-    return render_template("home.html")
+@app.route('/myself')
+def myself():
+    return render_template("myself.html", client_name=client_name, active1="active")
+
+@app.route('/services')
+def services():
+    return render_template("services.html", client_name=client_name, active2="active")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html", client_name=client_name, active3="active")
+
+
+
+
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,2 @@
+client_name = "Prince Dixit"
+
