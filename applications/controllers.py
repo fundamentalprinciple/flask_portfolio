7cc429a17b4ec01bb189226f509f3b35ab25c883
fundamentalprinciple <princedixit931@gmail.com>
2025-12-24 04:14:27 +0000

    Showcase section complete, need to fix resposiveness next, may implement custom CSS (flexbox)

--- applications/controllers.py
+++ applications/controllers.py
@@ -1,12 +1,12 @@
 from main import app
 from flask import render_template
 
-from static.variableFiles.variables import client_name, client_bio, projectInfo
+from static.variableFiles.variables import client_name, client_bio, showcase_type, showcaseInfo 
 
 @app.route('/')
 @app.route('/myself')
 def myself():
-    return render_template("myself.html", client_name=client_name, active1="active", client_bio=client_bio, title_project1=projectInfo[0][0], info_project1=projectInfo[0][1], link_project1=projectInfo[0][2])
+    return render_template("myself.html", client_name=client_name, active1="active", client_bio=client_bio, showcase_type=showcase_type , title_showcase1=showcaseInfo[0][0], info_showcase1=showcaseInfo[0][1], link_showcase1=showcaseInfo[0][2], title_showcase2=showcaseInfo[1][0], info_showcase2=showcaseInfo[1][1], link_showcase2=showcaseInfo[1][2], title_showcase3=showcaseInfo[2][0], info_showcase3=showcaseInfo[2][1], link_showcase3=showcaseInfo[2][2])
 
 @app.route('/services')
 def services():
--- static/variableFiles/variables.py
+++ static/variableFiles/variables.py
@@ -2,6 +2,8 @@ client_name = "Prince Dixit"
 
 client_bio = "A full stack Python & MERN web developer. I'm into computers and all related tech. I'm self-taught and currently enrolled at IIT Madras (B.S. in Data Science and Applications, second year)."
 
-projectInfo = [("Hospital Management System (Nov 2025):", "Developed a full-stack CRUD hospital management web application with role-based access controls. Integrated Flask-Security for authentication and implemented a doctor–patient interaction workflow. Tech: Python Flask, Flask-SQLAlchemy, Flask-Security, SQLite3, HTML/CSS/JS, Jinja2, Bootstrap.", "https://github.com/fundamentalprinciple/Hospital-Management-System")]
+showcase_type = "Projects"
+
+showcaseInfo = [("Hospital Management System (Nov 2025):", "Developed a full-stack CRUD hospital management web application with role-based access controls. Integrated Flask-Security for authentication and implemented a doctor–patient interaction workflow. Tech: Python Flask, Flask-SQLAlchemy, Flask-Security, SQLite3, HTML/CSS/JS, Jinja2, Bootstrap.", "https://github.com/fundamentalprinciple/Hospital-Management-System"), ("City weather Web App (Aug 2024):", "Developed and deployed (not live currently) a basic weather web app, implemented Flask for API calls to openweathermap.org. Tech: Flask, HTML/CSS & Jinja2.", "https://github.com/fundamentalprinciple/weather_flaskapp"), ("Portfolio WebApp for freelancing (Dec 2025):", "Developed a reproducible Portfolio webapp (recursion!) for freelancing purposes, implemented Bootstrap and custom CSS styling to make an elegant UI. Currently under development, will update this description later.", "https://github.com/fundamentalprinciple/flask_portfolio")]
 
 
